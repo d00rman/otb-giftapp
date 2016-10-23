dc8581381dcde2ce1d0eae05dbe40ccd5a27f18f
Marko Obrovac <marko.obrovac@gmail.com>
2016-10-23 08:41:59 +0300

    Admin: Add the add campaign route and template

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -6,13 +6,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class AdminController extends Controller
-{
+use AppBundle\Entity\Campaign;
+
+
+class AdminController extends Controller {
+
+
     /**
      * @Route("/admin", name="admin_index")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
+
         $campaigns = $this->getDoctrine()
         ->getRepository('AppBundle:Campaign')
         ->findAll();
@@ -20,5 +24,36 @@ class AdminController extends Controller
         return $this->render('admin/index.html.twig', array(
             'campaigns' => $campaigns
         ));
+
     }
+
+
+    /**
+     * @Route("/admin/campaign-add", name="admin_campaign_add")
+     */
+    public function campaignAdd(Request $request) {
+
+        if($request->getMethod() == 'GET') {
+            return $this->render('admin/campaign-add.html.twig');
+        }
+
+        $campaign = new Campaign();
+        $em = $this->getDoctrine()->getManager();
+
+        $vars = $request->request->all();
+        $vars['start_date'] = \DateTime::createFromFormat('d/m/Y', $vars['start_date']);
+        $vars['end_date'] = \DateTime::createFromFormat('d/m/Y', $vars['end_date']);
+
+        $campaign->setName($vars['name']);
+        $campaign->setYear($vars['year']);
+        $campaign->setStartDate($vars['start_date']);
+        $campaign->setEndDate($vars['end_date']);
+        $em->persist($campaign);
+        $em->flush();
+
+        return $this->redirectToRoute('admin_index');
+
+    }
+
+
 }
