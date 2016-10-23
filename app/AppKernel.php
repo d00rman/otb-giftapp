c2fcf6cf9be0480474d38482d4a1e43a6ff02562
Marko Obrovac <marko.obrovac@gmail.com>
2016-10-23 11:49:03 +0300

    Remove hwi/oauth-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,7 +8,6 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
-            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
